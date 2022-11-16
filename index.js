ff64db5f0fca9af328430eb90ec8d206ea61f086
AKSHAT <114349022+codeproak@users.noreply.github.com>
2022-11-16 20:48:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function hideload() {
+    document.getElementById("loadscreen").style.display="none";
+}
+
+function showmenu() {
+    document.getElementById("navlinks").style.display="block";
+    document.getElementById("navhidemenu").style.display="block";
+    document.getElementById("navshowmenu").style.display="none";
+}
+
+function hidemenu() {
+    document.getElementById("navlinks").style.display="none";
+    document.getElementById("navhidemenu").style.display="none";
+    document.getElementById("navshowmenu").style.display="block";
+}
